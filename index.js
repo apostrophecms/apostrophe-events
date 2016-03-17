19c9f68720e396d31dfdf733d65373b6606ddfb7
Kerry Gilbert <kerry@punkave.com>
2016-03-17 14:06:12 -0400

    events now broken out in to modules for events-pages and events-widgets. adding support to apostrophe to accept this as a bundle

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 module.exports = {
+  name: 'apostrophe-events',
+  alias: 'events',
+  label: 'Event',
   extend: 'apostrophe-pieces',
   
+  moogBundle: {
+    modules: [ 'apostrophe-events-pages', 'apostrophe-events-widgets' ],
+    directory: 'lib/modules'
+  },
 
   beforeConstruct: function(self, options) {
+    options.addFields = [
+      {
+        name: 'startDate',
+        label: 'Start Date',
+        type: 'date',
+        required: true
+      },
+      {
+        name: 'startTime',
+        label: 'Start Time',
+        type: 'time',
+        required: true
+      },
+      {
+        name: 'multiDay',
+        label: 'This is a multi-day event',
+        type: 'boolean',
+        def: false
+      }
+    ].concat(options.addFields || []);
 
+    console.log('events:beforeConstruct');
+    // return setImmediate(callback);
   },
 
   construct: function(self, options) {
-    
+    console.log('events:construct');
+    // return setImmediate(callback);
   },
 
-  afterConstruct: function(self, options) {
-    
+  afterConstruct: function(self) {
+    console.log('events:afterConstruct');
+    // return setImmediate(callback);
   }
 };
