fd27a753c8699c6e79cedb676cdb890de3e7acc3
Kerry Gilbert <kerry@punkave.com>
2016-03-18 16:59:33 -0400

    first pass at pulling together basic events functionality. added base configuration, created schema fields specific to events and extended the cursor to support an upcoming flag which fetches present/future events only

--- index.js
+++ index.js
@@ -1,47 +1,103 @@
+var _ = require('lodash');
+
 module.exports = {
-  name: 'apostrophe-events',
+  name: 'apostrophe-event',
   alias: 'events',
   label: 'Event',
   extend: 'apostrophe-pieces',
   
   moogBundle: {
-    modules: [ 'apostrophe-events-pages', 'apostrophe-events-widgets' ],
+    modules: ['apostrophe-events-pages', 'apostrophe-events-widgets'],
     directory: 'lib/modules'
   },
 
   beforeConstruct: function(self, options) {
+    options.sort = { startDate: 1 };
+
     options.addFields = [
       {
         name: 'startDate',
-        label: 'Start Date',
+        label: 'Date',
         type: 'date',
         required: true
       },
       {
+        name: 'allDay',
+        label: 'Is this an all day event?',
+        type: 'select',
+        choices: [
+          { label: 'Yes', value: '1' },
+          { label: 'No', value: '0', showFields: ['startTime', 'endTime'] }
+        ],
+        def: 0
+      },      
+      {
         name: 'startTime',
         label: 'Start Time',
         type: 'time',
+        def: '9am',
         required: true
       },
       {
-        name: 'multiDay',
-        label: 'This is a multi-day event',
-        type: 'boolean',
-        def: false
+        name: 'endTime',
+        label: 'End Time',
+        type: 'time',
+        def: '5:30pm',
+        required: true        
+      },
+      {
+        name: 'dateType',
+        label: 'What type of event is this?',
+        type: 'select',
+        choices: [
+          { label: 'Single Day', value: 'single' },
+          { label: 'Consecutive Days', value: 'consecutive', showFields: ['endDate'] },
+          { label: 'Recurring', value: 'repeat', showFields: ['repeatInterval', 'repeatCount'] },
+        ],
+        def: 'single'
+      },
+      {
+        name: 'endDate',
+        label: 'End Date',
+        type: 'date'
+      },
+      {
+        name: 'repeatInterval',
+        label: 'Repeats every',
+        type: 'select',
+        choices: [
+          { label: 'Week', value: 'week' },
+          { label: 'Month', value: 'month' }
+        ]
+      },
+      {
+        name: 'repeatCount',
+        label: 'Repeats how many times?',
+        type: 'integer',
+        def: 1
       }
     ].concat(options.addFields || []);
 
-    console.log('events:beforeConstruct');
-    // return setImmediate(callback);
+    options.arrangeFields = _.merge([
+      { name: 'basic', label: 'Basics', fields: ['title', 'slug', 'startDate', 'allDay', 'startTime', 'endTime'] },
+      { name: 'advanced', label: 'Advanced', fields: ['dateType', 'endDate', 'repeatInterval', 'repeatCount'] },
+      { name: 'meta', label: 'Meta', fields: ['tags','published'] }
+    ], options.arrangeFields || []);
   },
 
   construct: function(self, options) {
-    console.log('events:construct');
-    // return setImmediate(callback);
-  },
+    var superFind = self.find;
 
-  afterConstruct: function(self) {
-    console.log('events:afterConstruct');
-    // return setImmediate(callback);
-  }
+    self.find = function(req, criteria, projection) {
+      var cursor = superFind(req, criteria, projection);
+      require('./lib/cursor')(self, cursor);
+      return cursor;
+    };
+
+    self.beforeSave = function(req, piece, callback) {
+      // Parse our dates and times 
+      piece.start = new Date(piece.startDate +'T'+ piece.startTime);
+      return callback(null);
+    };
+  },
 };
